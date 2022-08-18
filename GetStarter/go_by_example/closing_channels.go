5e88ae66b8afbe6e3eda45d5cf592fa0d9982a29
Leo <45577227+leozanchett@users.noreply.github.com>
2022-08-18 09:25:56 -0300

    Create closing_channels.go

---
+++ GetStarter/go_by_example/closing_channels.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("sent all jobs")
+	jobs <- 4
+	time.Sleep(2 * time.Second)
+	close(jobs)
+
+	<-done
+}

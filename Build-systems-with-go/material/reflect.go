d47295fa410bc28dcd55e1ab1f3b4734c2c8fcfc
Leo <45577227+leozanchett@users.noreply.github.com>
2022-08-23 12:02:32 -0300

    Create reflect.go

---
+++ Build-systems-with-go/material/reflect.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type T struct {
+	A int32
+	B string
+}
+
+func main() {
+	t := T{42, "forty two"}
+	typeT := reflect.TypeOf(t)
+	fmt.Println(typeT)
+	for i := 0; i < typeT.NumField(); i++ {
+		field := typeT.Field(i)
+		fmt.Printf("%s: %s\n", field.Name, field.Type)
+	}
+}

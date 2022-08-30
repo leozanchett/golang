f777ea01e58eaa29cb392fb5e4428c159aad999f
Leo <45577227+leozanchett@users.noreply.github.com>
2022-08-30 11:54:23 -0300

    Create other_http_methods.go

---
+++ Build-systems-with-go/material/http/other_http_methods.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	bodyRequest := []byte(`"user":"john", "email":"john@gmail.com"`)
+	bufferBody := bytes.NewBuffer(bodyRequest)
+	const URL string = "https://httpbin.org/put"
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
+	header.Add("X-Custom-Header", "somevalue")
+	header.Add("User-Agent", "safe-the-world-with-go")
+
+	req, err := http.NewRequest(http.MethodPut, URL, bufferBody)
+	if err != nil {
+		panic(err)
+	}
+	req.Header = header
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
+	bodyAnswer := bufio.NewScanner(resp.Body)
+	for bodyAnswer.Scan() {
+		fmt.Println(bodyAnswer.Text())
+	}
+}

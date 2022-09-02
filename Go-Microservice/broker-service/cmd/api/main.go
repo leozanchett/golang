b2a1814f399f72731a121d333ea7b8b3c1c372a8
Leo <45577227+leozanchett@users.noreply.github.com>
2022-09-02 15:02:04 -0300

    Broker-Service

--- Go-Microservice/broker-service/cmd/api/main.go
+++ Go-Microservice/broker-service/cmd/api/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "81"
+
+type Config struct{}
+
 func main() {
+	app := Config{}
+	log.Println("Starting the broker:", webPort)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 
 }

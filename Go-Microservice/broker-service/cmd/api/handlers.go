78924b44cc10b73da46ed6d73390e68c6d7c4701
Leo <45577227+leozanchett@users.noreply.github.com>
2022-09-20 13:33:49 -0300

    broker, update handlers to adapt e-mail

--- Go-Microservice/broker-service/cmd/api/handlers.go
+++ Go-Microservice/broker-service/cmd/api/handlers.go
@@ -11,6 +11,14 @@ type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
 	Log    LogPayLoad  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 type AuthPayload struct {
@@ -46,6 +54,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
 		app.logItem(w, requestPayload.Log)
+	case "mail":
+		app.sendMail(w, requestPayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("invalid action"))
 	}
@@ -133,4 +143,37 @@ func (app *Config) authenticate(w http.ResponseWriter, auth AuthPayload) {
 	payload.Data = responsePayload.Data
 
 	app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
+	jsonData, _ := json.MarshalIndent(msg, "", "\t")
+
+	// call the mail service
+	const MAIL_SERVICE_URL = "http://mail-service:3000/send"
+	request, err := http.NewRequest("POST", MAIL_SERVICE_URL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back the correct status code
+	if response.StatusCode != http.StatusAccepted && response.StatusCode != http.StatusOK {
+		app.errorJSON(w, errors.New("mail service returned an error"))
+		return
+	}
+
+	// send back json
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Mail sent to " + msg.To
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
